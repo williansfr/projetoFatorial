0401afcb42749a6c297d287fe812e55af87eb383
Willians Ferreira Rocha <williansfr@hotmail.com>
2017-05-31 19:10:10 -0300

    Exceção Negativos

--- Fatorial.java
+++ Fatorial.java
@@ -1,6 +1,8 @@
 public class Fatorial {
 	// Versao Iterativa
-	public  int calcularFatorial(int n) {
+	public  int calcularFatorial(int n) throws IllegalArgumentException {
+		if (n < 0)
+			throw new IllegalArgumentException("Numero Negativo");
 		int resultado = 1;
 		for (int i = 2; i <= n; i++)
 			resultado *= i;
