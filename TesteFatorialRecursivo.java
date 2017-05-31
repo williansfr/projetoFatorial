6aac21c1753d8f07f3a9d7b43007c7eed75b40a3
Willians Ferreira Rocha <williansfr@hotmail.com>
2017-05-31 09:01:33 -0300

    TesteFatorialRecursivo removido

--- TesteFatorialRecursivo.java
+++
@@ -1,9 +0,0 @@
-public class TesteFatorialRecursivo
-{
-	public static void main(String [] args)
-	{
- 		FatorialRecursivo fat = new FatorialRecursivo();
-		int fatorial = fat.calcularFatorial(5);
-		System.out.println(fatorial);
-	}
-}
