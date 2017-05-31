e032670caa41ded0832b7354d6dc6dbb841c9d63
Willians Ferreira Rocha <williansfr@hotmail.com>
2017-05-31 08:35:34 -0300

    Teste 4

--- Fatorial.java
+++ Fatorial.java
@@ -1,4 +1,5 @@
 public class Fatorial {
+	// Versao Iterativa
 	public  int calcularFatorial(int n) {
 		int resultado = 1;
 		for (int i = 2; i <= n; i++)
--- TesteFatorial.java
+++ TesteFatorial.java
@@ -3,6 +3,7 @@ public class TesteFatorial
 	public static void main(String [] args)
 	{
  		Fatorial fat = new Fatorial();
-		System.out.println(fat.calcularFatorial(5));
+		int fatorial = fat.calcularFatorial(5);
+		System.out.println(fatorial);
 	}
 }
